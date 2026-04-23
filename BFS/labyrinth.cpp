f08e1b18387a0d9741000dfdb0928a1cce4d28f0
Md. Jamil Uddin <jamiluddin1627@gmail.com>
2026-04-23 17:16:45 +0600

    Jamil

--- BFS/labyrinth.cpp
+++ BFS/labyrinth.cpp
@@ -1,41 +1,59 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N = 100;
+
+const int N = 1000;
 int maze[N][N];
 bool visited[N][N];
+
+pair<int,int> parent[N][N];
+char pathDir[N][N];
+
 int n, m;
-int dx[] = {0, 0, -1, 1}; // Right, Left, Up, Down
-int dy[] = {1, -1, 0, 0}; // Right, Left, Up, Down
+
+int dx[] = {0, 0, -1, 1};
+int dy[] = {1, -1, 0, 0};
+char dir[] = {'R', 'L', 'U', 'D'};
+
 bool isValid(int x, int y)
 {
     return (x >= 0 && x < n && y >= 0 && y < m && maze[x][y] != -1 && !visited[x][y]);
 }
+
 void bfs(pair<int, int> src)
 {
     queue<pair<int, int>> q;
     q.push(src);
     visited[src.first][src.second] = true;
+
     while(!q.empty())
     {
         pair<int, int> cell = q.front();
         q.pop();
-        //cout << cell.first << " " << cell.second << endl; // Process the current cell (e.g., print its coordinates)
+
         for(int i = 0; i < 4; i++)
         {
             int newX = cell.first + dx[i];
             int newY = cell.second + dy[i];
+
             if(isValid(newX, newY))
             {
                 visited[newX][newY] = true;
+
+                parent[newX][newY] = cell;     // add
+                pathDir[newX][newY] = dir[i];  // add
+
                 q.push({newX, newY});
             }
         }
     }
 }
+
 int main()
 {
     cin >> n >> m;
+
     pair<int, int> src, dest;
+
     for(int i = 0; i < n; i++)
     {
         string row;
@@ -44,20 +62,37 @@ int main()
         {
             if(row[j] == '#') maze[i][j] = -1;
             else maze[i][j] = 0;
+
             if(row[j] == 'A') src = {i, j};
             if(row[j] == 'B') dest = {i, j};
         }
     }
+
     memset(visited, false, sizeof(visited));
+
     bfs(src);
-    if(visited[dest.first][dest.second])
+
+    if(!visited[dest.first][dest.second])
     {
-        cout << "Path found!" << endl;
+        cout << "NO\n";
         return 0;
     }
-    else
+
+    // path build (new add)
+    string path;
+    pair<int,int> cur = dest;
+
+    while(cur != src)
     {
-        cout << "No path found." << endl;
+        path += pathDir[cur.first][cur.second];
+        cur = parent[cur.first][cur.second];
     }
+
+    reverse(path.begin(), path.end());
+
+    cout << "YES\n";
+    cout << path.size() << "\n";
+    cout << path << "\n";
+
     return 0;
 }
--- DSU/dsu_imp.cpp
+++ DSU/dsu_imp.cpp
@@ -3,12 +3,14 @@ using namespace std;
 const int N = 1e5 + 10;
 int parent[N];
 int group_size[N];
+int level[N]; //optional, can be used to store the level or rank of each set for union by rank optimization
 void dsu_set(int n)
 {
     for(int i = 0; i < n; i++)
     {
         parent[i] = -1;
         group_size[i] = 1;
+        level[i] = 0; //initialize the level/rank of each set to 0
     }
 }
 int find_set(int u)
@@ -34,18 +36,49 @@ void union_set(int a, int b)
         group_size[leaderB] += group_size[leaderA]; //update the size of the set represented by leaderB
     }
 }
+void union_set_by_rank(int a, int b)
+{
+    int leaderA = find_set(a); //find the representative of the set to which a belongs
+    int leaderB = find_set(b); //find the representative of the set to which b belongs
+    if(level[leaderA] > level[leaderB])
+    {
+        parent[leaderB] = leaderA; //make leaderA the parent of leaderB
+        group_size[leaderA] += group_size[leaderB]; //update the size of the set represented by leaderA
+    }
+    else if(level[leaderA] < level[leaderB])
+    {
+        parent[leaderA] = leaderB; //make leaderB the parent of leaderA
+        group_size[leaderB] += group_size[leaderA]; //update the size of the set represented by leaderB
+    }
+    else
+    {
+        parent[leaderA] = leaderB; //make leaderB the parent of leaderA
+        group_size[leaderB] += group_size[leaderA]; //update the size of the set represented by leaderB
+        level[leaderB]++; //increment the level/rank of the new representative (leaderB) since both sets had the same level/rank
+    }
+}
 int main()
 {
     dsu_set(7); //initialize DSU for 7 elements (0 to 6)
-    union_set(1, 2); //union the sets containing 1 and 2
-    union_set(2, 3); //union the sets containing 2 and 3
-    union_set(4, 5); //union the sets containing 4 and 5
-    union_set(5, 6); //union the sets containing 5 and 6
+    // union_set(1, 2); //union the sets containing 1 and 2
+    // union_set(2, 3); //union the sets containing 2 and 3
+    // union_set(4, 5); //union the sets containing 4 and 5
+    // union_set(5, 6); //union the sets containing 5 and 6
+    // cout << "Representative of set containing 1: " << find_set(1) << endl; //should print the representative of the set containing 1
+    // cout << "Representative of set containing 4: " << find_set(4) << endl; //should print the representative of the set containing 4
+    // cout << "Representative of set containing 0: " << find_set(0) << endl; //should print the representative of the set containing 0 (which is itself)
+    // cout << "Representative of set containing 3: " << find_set(3) << endl; //should print the representative of the set containing 3 (which should be the same as that of 1 and 2)
+    // union_set(3, 4); //union the sets containing 3 and 4 (which will also union the sets containing 1, 2, 5, and 6)
+    // cout << "Representative of set containing 3 after union with 4: " << find_set(3) << endl;
+    union_set_by_rank(1, 2); //union the sets containing 1 and 2 using union by rank
+    union_set_by_rank(2, 3); //union the sets containing 2 and 3 using union by rank
+    union_set_by_rank(4, 5); //union the sets containing 4 and 5 using union by rank
+    union_set_by_rank(5, 6); //union the sets containing 5 and 6 using union by rank
     cout << "Representative of set containing 1: " << find_set(1) << endl; //should print the representative of the set containing 1
     cout << "Representative of set containing 4: " << find_set(4) << endl; //should print the representative of the set containing 4
     cout << "Representative of set containing 0: " << find_set(0) << endl; //should print the representative of the set containing 0 (which is itself)
     cout << "Representative of set containing 3: " << find_set(3) << endl; //should print the representative of the set containing 3 (which should be the same as that of 1 and 2)
-    union_set(3, 4); //union the sets containing 3 and 4 (which will also union the sets containing 1, 2, 5, and 6)
+    union_set_by_rank(3, 4); //union the sets containing 3 and 4 (which will also union the sets containing 1, 2, 5, and 6) using union by rank
     cout << "Representative of set containing 3 after union with 4: " << find_set(3) << endl;
     return 0;
 }
